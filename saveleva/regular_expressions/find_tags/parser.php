ffdb22bd7cb743c15f7840da8f61c7425bdd872c
sukhanov <webdevforbiz@gmail.com>
2019-09-13 17:12:02 +0300

    find tags

--- saveleva/regular_expressions/find_tags/parser.php
+++ saveleva/regular_expressions/find_tags/parser.php
@@ -1,1 +1,16 @@
 <?php
+
+$string = file_get_contents('source.html');
+echo $string;
+$pattern = "/<(!|\/|\w)(\w*||\w*\s*\S+)(\s*>|>)/m"; // Выбрать все теги без контента
+
+
+$count = preg_match_all( $pattern, $string, $result );
+
+for( $i = 0 ; $i < $count; $i++ ){
+    echo htmlspecialchars( $result[0][$i]) . '<br>';
+}
+
+//echo '<pre>';
+//print_r( $result );
+//echo '</pre>';
